71324a474d9383d045991af187040437bc715826
harold jhan <haroldzhen531@gmail.com>
2021-08-04 00:10:35 +0800

    Update 2021-08-03T16:10:31.682Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,11 @@
 "use strict";
 
+/* global ClassicEditor   */
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
+});
+ClassicEditor.create(document.querySelector("#editor"))["catch"](function (error) {
+  console.error(error);
 });
 //# sourceMappingURL=all.js.map
